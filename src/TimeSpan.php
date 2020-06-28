3e22e165535d6667d6faab5c55af13a0d7c34b24
jacobfriis <jacob.friis@simplecomplex.net>
2020-06-28 19:24:50 +0200

    TimeSpan: converting args to inner Times; which is not really necessary nor sensible.

--- src/TimeSpan.php
+++ src/TimeSpan.php
@@ -28,7 +28,7 @@ class TimeSpan
     protected $timezoneName;
 
     /**
-     * @var TimeImmutable
+     * @var Time
      */
     protected $fromTime;
 
@@ -39,7 +39,7 @@ class TimeSpan
 
     /**
      *
-     * @var TimeImmutable
+     * @var Time
      */
     protected $toTime;
 
@@ -48,29 +48,29 @@ class TimeSpan
      */
     protected $toUnixMicroseconds;
 
-
-    /**
-     * @var TimeInterval
-     */
-    protected $interval;
-
     /**
      * Difference is allowed to be zero, but not negative.
      *
-     * External change to arg $fromDate or $toDate aren't reflected internally;
-     * saves as TimeImmutable.
+     * Saves references to arg $fromDate, $toDate if they are Time instances;
+     * otherwise saves equivalent Time|TimeImmutable objects.
+     * @see TimeSpan::$fromTime
+     * @see TimeSpan::$toTime
      *
      * @param \DateTimeInterface $fromDate
      * @param \DateTimeInterface $toDate
+     *      Must be equal to or later than arg $fromDate.
+     *      Timezone must be equal to $fromDate's.
      *
      * @throws \Exception
      *      Propagated.
      */
     public function __construct(\DateTimeInterface $fromDate, \DateTimeInterface $toDate)
     {
+        // @todo: there's no reason to convert to Time.
+
+
         if ($fromDate instanceof Time) {
-            $this->fromTime = $fromDate instanceof TimeImmutable ? $fromDate :
-                TimeImmutable::createFromMutable($fromDate);
+            $this->fromTime = $fromDate;
         }
         elseif ($fromDate instanceof \DateTimeImmutable) {
             $this->fromTime = TimeImmutable::createFromImmutable($fromDate);
@@ -81,8 +81,7 @@ class TimeSpan
         $this->timezoneName = $this->fromTime->timezoneName;
 
         if ($toDate instanceof Time) {
-            $this->toTime = $toDate instanceof TimeImmutable ? $toDate :
-                TimeImmutable::createFromMutable($toDate);
+            $this->toTime = $toDate;
         }
         elseif ($toDate instanceof \DateTimeImmutable) {
             $this->toTime = TimeImmutable::createFromImmutable($toDate);
@@ -94,8 +93,8 @@ class TimeSpan
         $to_tz = $this->toTime->timezoneName;
         if ($to_tz != $this->timezoneName) {
             throw new \InvalidArgumentException(
-                'Arg $fromDate timezone[' . $this->fromTime
-                . '] differs from arg $toDate timezone[' . $this->timezoneName . '].'
+                'Arg $toDate timezone[' . $to_tz
+                . '] differs from arg $fromDate timezone[' . $this->timezoneName . '].'
             );
         }
 
@@ -109,22 +108,29 @@ class TimeSpan
                 'Arg $fromDate[' . $this->fromTime . '] cannot be later than arg $toDate[' . $this->toTime . '].'
             );
         }
-//        $this->interval = $this->fromTime->diffTime($this->toTime);
-//        if ($this->interval->totalMicroseconds < 0) {
-//            throw new \InvalidArgumentException(
-//                'Arg $fromDate[' . $this->fromTime . '] cannot be later than arg $toDate[' . $this->toTime . '].'
-//            );
-//        }
     }
 
     /**
+     * @return TimeInterval
+     *
+     * @throws \Exception
+     *      Propagated.
+     */
+    public function interval() : TimeInterval
+    {
+        return $this->fromTime->diffTime($this->toTime);
+    }
+
+    /**
+     * Measures diff between this toTime and that fromTime.
+     *
      * @param TimeSpan $timeSpan
      * @return TimeInterval
      *
      * @throws \Exception
      *      Propagated.
      */
-    public function diffTime(TimeSpan $timeSpan) : TimeInterval
+    public function diffTimeSpan(TimeSpan $timeSpan) : TimeInterval
     {
         if ($timeSpan->timezoneName != $this->timezoneName) {
             throw new \InvalidArgumentException(
@@ -135,10 +141,90 @@ class TimeSpan
         return $this->toTime->diffTime($timeSpan->fromTime);
     }
 
+    /**
+     * @var int
+     */
+    public const OVERLAP_NONE = 0;
 
+    /**
+     * @var int
+     */
+    public const OVERLAP_IDENTITY = 1;
+
+    /**
+     * @var int
+     */
+    public const OVERLAP_ENCLOSES = 2;
+
+    /**
+     * @var int
+     */
+    public const OVERLAP_IS_SUBSET = 3;
+
+    /**
+     * @var int
+     */
+    public const OVERLAP_ENDS_WITHIN = 4;
+
+    /**
+     * @var int
+     */
+    public const OVERLAP_BEGINS_WITHIN = 5;
+
+
+    /**
+     * Check whether another TimeSpan overlaps this.
+     *
+     * @see TimeSpan::OVERLAP_NONE
+     * @see TimeSpan::OVERLAP_IDENTITY
+     * @see TimeSpan::OVERLAP_ENCLOSES
+     * @see TimeSpan::OVERLAP_IS_SUBSET
+     * @see TimeSpan::OVERLAP_ENDS_WITHIN
+     * @see TimeSpan::OVERLAP_BEGINS_WITHIN
+     *
+     * @param TimeSpan $timeSpan
+     *
+     * @return int
+     *
+     * @throws \LogicException
+     *      Algo error in this method; failure to explain overlap.
+     */
     public function overlap(TimeSpan $timeSpan)
     {
-        return 0;
+        $subject_from = $timeSpan->fromTime->unixMicroseconds;
+        $subject_to = $timeSpan->toTime->unixMicroseconds;
+
+        if ($subject_to >= $this->fromUnixMicroseconds && $subject_from <= $this->toUnixMicroseconds) {
+            // Identity.
+            if ($subject_from == $this->fromUnixMicroseconds && $subject_to == $this->toUnixMicroseconds) {
+                return static::OVERLAP_IDENTITY;
+            }
+            // Encloses this.
+            elseif ($subject_from <= $this->fromUnixMicroseconds && $subject_to >= $this->toUnixMicroseconds) {
+                return static::OVERLAP_ENCLOSES;
+            }
+            // Subset of this.
+            elseif ($subject_from >= $this->fromUnixMicroseconds && $subject_to <= $this->toUnixMicroseconds) {
+                return static::OVERLAP_IS_SUBSET;
+            }
+            // Ends within this.
+            elseif ($subject_to < $this->toUnixMicroseconds) {
+                return static::OVERLAP_ENDS_WITHIN;
+            }
+            // Begins within this.
+            elseif ($subject_from > $this->fromUnixMicroseconds) {
+                return static::OVERLAP_BEGINS_WITHIN;
+            }
+            else {
+                throw new \LogicException(
+                    __CLASS__ . '::' . __METHOD__ . '() fails to explain how '
+                    . $timeSpan->fromTime . ' - ' . $timeSpan->toTime
+                    . ' overlaps ' . $this->fromTime . ' - ' . $this->toTime . '.'
+                );
+            }
+        }
+
+        return static::OVERLAP_NONE;
     }
 
     /**
